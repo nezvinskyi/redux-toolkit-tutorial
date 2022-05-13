560f97573628f8539a54871e461ed9914b3275ca
Dmitry Nezvinskyi <nezvinskyi@gmail.com>
2022-05-13 17:24:51 +0200

    initial commit

---
+++ starter/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { CartIcon } from "../icons";
+import { useSelector } from "react-redux";
+
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>redux toolkit</h3>
+        
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
